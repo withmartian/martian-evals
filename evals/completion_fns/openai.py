1eba794d4377bb74a020ed5a7959182ae3b5c6e8
Jacob Bieker <jacob@bieker.tech>
2024-01-29 08:31:25 +0000

    Add changes so all prompt roles are 'user' since some models don't support assistant or system

--- evals/completion_fns/openai.py
+++ evals/completion_fns/openai.py
@@ -126,7 +126,12 @@ class OpenAIChatCompletionFn(CompletionFnSpec):
             )
 
         openai_create_prompt: OpenAICreateChatPrompt = prompt.to_formatted_prompt()
-
+        # change the "role": "system" to "role": "user"
+        for i in range(len(openai_create_prompt)):
+            if openai_create_prompt[i]["role"] == "system":
+                openai_create_prompt[i]["role"] = "user"
+            elif openai_create_prompt[i]["role"] == "assistant":
+                openai_create_prompt[i]["role"] = "user"
         result = openai_chat_completion_create_retrying(
             OpenAI(api_key=self.api_key, base_url=self.api_base),
             model=self.model,
