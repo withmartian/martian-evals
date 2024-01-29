5080bdc39c1bed4be364dbef3477eaae2ecc2ccd
Jacob Bieker <jacob@bieker.tech>
2024-01-29 08:30:48 +0000

    Add changes for running subsample and kshot on Hellaswag and MMLU

--- evals/elsuite/multiple_choice.py
+++ evals/elsuite/multiple_choice.py
@@ -16,6 +16,43 @@ class Sample(BaseModel):
     answers: list[str]
     label: int
 
+def get_choices():
+    return ["A", "B", "C", "D"]
+
+
+def format_example(example, include_answer=True):
+    prompt = example["question"]
+    for j in range(4):
+        prompt += "\n{}) {}".format(get_choices()[j], example["choices"][j])
+    prompt += "\n"
+    if include_answer:
+        prompt += " {}\n\n".format(get_choices()[example["answer"]])
+    return prompt
+
+
+def format_example_hellaswag(example, include_answer=True):
+    prompt = example["ctx"]
+    for j in range(4):
+        prompt += "\n{}) {}".format(get_choices()[j], example["endings"][j])
+    prompt += "\n"
+    if include_answer:
+        prompt += " {}\n\n".format(get_choices()[int(example["label"])])
+    return prompt
+
+
+def gen_prompt(train_dataset, k=-1):
+    prompt = "The following are multiple choice questions (with answers).\n\n"
+    for i in range(k):
+        prompt += format_example(next(train_dataset))
+    return prompt
+
+
+def gen_prompt_hellaswag(train_dataset, k=-1):
+    prompt = "Choose the most plausible continuation for the story. \n\nThe following are example stories and continuations (with answers).\n\n"
+    for i in range(k):
+        prompt += format_example_hellaswag(next(train_dataset))
+    return prompt
+
 
 def get_dataset(url: str) -> list[Sample]:
     parsed = urlparse(url)
@@ -24,8 +61,14 @@ def get_dataset(url: str) -> list[Sample]:
         query = {k: v[0] for k, v in query.items()}
 
         path = parsed.netloc
-
+        if path == "hellaswag":
+            if query.get("split") == "validation":
+                query["split"] = "validation[:1%]"
+        else:
+            if query.get("split") == "validation":
+                query["split"] = "validation[:5%]"
         dataset = load_dataset(path, **query)
+
         if path == "hellaswag":
             return [
                 Sample(
@@ -61,6 +104,15 @@ class MultipleChoice(evals.Eval):
         assert len(completion_fns) == 1, "MultipleChoice only supports one completion fn"
         self.dataset = dataset
         self.instructions = instructions
+        if "hendrycks" in self.dataset:
+            dataset = load_dataset("cais/mmlu", 'all', split="dev",)
+            # Has the same start at the defaultMMLU_MSG but with the 5 shot examples
+            self.instructions = gen_prompt(iter(dataset), k=5)
+        elif "hellaswag" in self.dataset:
+            dataset = load_dataset("hellaswag", split="train")
+            self.instructions = gen_prompt_hellaswag(iter(dataset), k=10)
+        else:
+            self.instructions = self.instructions
 
     def eval_sample(self, sample, rng):
         assert isinstance(sample, Sample)
